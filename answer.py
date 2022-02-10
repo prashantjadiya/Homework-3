c792cbd95d8052d63ecf62a85cc1e5f478a79ee0
Prashant Jadiya <42677306+prashantjadiya@users.noreply.github.com>
2022-02-10 03:35:10 -0500

    Initial commit

---
+++ answer.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+"""
+Python Core object Types
+"""
+
+
+def tuple():
+    """
+    This is to review tuples and basic operations.
+    """
+    # Assign the first element of the tuple "t1" to x
+    t1 = (13, 100, 99, 100, 999)
+
+    x = 
+
+    # Print the second from the last element of the tuple "t1".
+    y = 
+
+    
+    #What's the index of 2 in the tuple "t2"?
+    t2 = (55, 777, 54, 6, 76, 101, 1, 2, 8679, 98, 123, 98, 98)
+    z = 
+
+    #How many times does 98 occur in the tuple "t2"?
+    f = 
+
+    # What is the sum of all the numbers in the tuple "t2"?
+    
+    m = 
+
+
+    # What is the maximum value in the tuple "t2"?
+    n = 
+
+    return x, y, z, f, m, n
+
+
+
+tuple()
